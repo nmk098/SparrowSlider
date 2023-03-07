6147d8a2150814e2cedadff30bdab940d1f31af2
nmk098 <nkuryumov@gmail.com>
2023-03-07 17:15:16 +0400

    Initial Commit

---
+++ SparrowSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SparrowSlider
+//
+//  Created by Никита Курюмов on 07.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
